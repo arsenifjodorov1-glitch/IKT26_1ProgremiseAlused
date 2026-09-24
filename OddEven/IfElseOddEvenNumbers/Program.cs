71690bc4b83597bbbb82c6619c21e494e2faef72
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-24 11:45:22 +0300

    q

---
+++ OddEven/IfElseOddEvenNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace IfElseOddAndEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //konsool küsib numbrit
+            //number tuleb ära parsida
+
+            //if ja else juures toimub kontroll
+            //kas on paaris või paaritu nr
+
+            string nr = Console.ReadLine();
+            int number = int.Parse(nr);
+
+            //% jagab muutujat number 2-ga kuni võrdub 0
+            //kui 2 asemel panna 3, siis jagab 3-ga
+            if(number %2 == 0)
+            {
+                Console.WriteLine("See on paarisarv " + number);
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paarisarv
+                EvenNumberMethod();
+            }
+            else 
+            {
+                Console.WriteLine("See on paaritu number " + number);
+                //siia tuleb välja kutsuda meetod,
+                //mis ütleb, et see on paarituarv
+                OddNumberMethod();
+            }
+        }
+
+        static void EvenNumberMethod()
+        {
+            Console.WriteLine("See on paarisarv");
+        }
+        static void OddNumberMethod()
+        {
+            Console.WriteLine("See on paarituarv");
+        }
+    }
+}
